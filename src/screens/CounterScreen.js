c5cc685c981b924cfb7ed2b9219ad81d1e32d529
andrewphilippov <a@fandb.pro>
2020-06-12 13:53:00 +0300

    Added useReducer to counter screen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,25 @@
-import React, { useState }                from 'react'
+import React, { useReducer }              from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+const ourReducer = (state, action) => {
+	switch (action.type) {
+		case 'increase':
+			return { ...state, counter: state.counter + action.payload }
+		case 'decrease':
+			return { ...state, counter: state.counter + action.payload }
+		default:
+			return state
+	}
+}
 const CounterScreen = () => {
-	const [counter, setCounter] = useState(0)
+	const [state, dispatch] = useReducer(ourReducer, { counter: 0 })
 	return (
 		<View>
 			<Button title='Increase'
-					onPress={ () => {
-						setCounter(counter + 1)
-					} } />
+					onPress={ () => dispatch({ type: 'increase', payload: 1 }) } />
 			<Button title='Decrease'
-					onPress={ () => {
-						setCounter(counter - 1)
-					} } />
-			<Text>CurrentCount: { counter }</Text>
+					onPress={ () => dispatch({ type: 'decrease', payload: -1 }) } />
+			<Text>CurrentCount: { state.counter }</Text>
 		</View>
 	)
 }
